022a3050f4f92671c0a3044da39ffc49df300279
yzgyjyw <yzgyjyw@163.com>
2019-01-13 11:52:23 +0800

    redis支持增刪改查

--- src/main/java/com/gs/utils/RedisUtils.java
+++ src/main/java/com/gs/utils/RedisUtils.java
@@ -1,8 +1,12 @@
 package com.gs.utils;
 
+import com.alibaba.fastjson.JSONObject;
+import com.gs.bean.News;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.Date;
 
 public class RedisUtils {
 
@@ -31,6 +35,36 @@ public class RedisUtils {
         return jedisPool.getResource();
     }
 
+
+    public static void insertNews(News news){
+        String newsStr = JSONObject.toJSONString(news);
+        Jedis jedis = getJedis();
+        jedis.hset("news",String.valueOf(news.getId()),newsStr);
+        close(jedis);
+    }
+
+    public static News getNewsByID(String id){
+        Jedis jedis = getJedis();
+        String newsStr = jedis.hget("news", id);
+        News news = JSONObject.parseObject(newsStr, News.class);
+        close(jedis);
+        return news;
+    }
+
+    public static void deleteByID(String id){
+        Jedis jedis = getJedis();
+        jedis.hdel("news",id);
+        close(jedis);
+    }
+
+    public static void updateByID(News news){
+        Jedis jedis = getJedis();
+        jedis.hset("news",String.valueOf(news.getId()),JSONObject.toJSONString(news));
+        close(jedis);
+    }
+
+
+
     //每次拿取jedis实例使用完后，需要返回到资源池
     public static void close(Jedis jedis){
         if(jedisPool==null){
@@ -40,7 +74,23 @@ public class RedisUtils {
     }
 
     public static void main(String[] args) {
-        getJedis().lpush("a","c");
+        News news = new News();
+        news.setPubTime(new Date());
+        news.setId(1);
+        news.setAuthor("jyw");
+        news.setContent("first news");
+        news.setTitle("abc");
+        news.setAbstracts("摘要");
+
+        insertNews(news);
+
+        News id = getNewsByID("1");
+
+
+        news.setAuthor("tl");
+        updateByID(news);
+
+        deleteByID("id");
     }
 
 }
