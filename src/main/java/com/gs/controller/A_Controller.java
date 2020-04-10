0c0c3f36328aa762540cd83a9e4572aeaa9e86d1
涂亮 <tuliang@corp.netease.com>
2020-04-10 22:10:21 +0800

    fix

---
+++ src/main/java/com/gs/controller/A_Controller.java
@@ -0,0 +1,11 @@
+package com.gs.controller;
+
+/**
+ * @author: 涂亮 （tuliang@corp.netease.com）
+ * @date: 2020/4/10 10:09 PM
+ **/
+public class A_Controller {
+    public static void main(String[] args) {
+        System.out.println("TESTA");
+    }
+}
